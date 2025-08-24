adc094b2517d048fcbbf43b1f1d6d5ebff91af36
Shravan55555 <134358009+Shravan55555@users.noreply.github.com>
2025-08-24 17:09:50 +0530

    Create axion.sh

---
+++ axion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+rm -rf .repo/local_manifests && \
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-23.0 --git-lfs && \
+git clone https://github.com/shravansayz/local_manifests.git --depth 1 -b axion .repo/local_manifests && \
+/opt/crave/resync.sh && \
+repo forall -c 'git lfs pull' && \
+export BUILD_USERNAME=shravan && \
+export BUILD_HOSTNAME=android-build && \
+export TZ=Asia/Kolkata && \
+source build/envsetup.sh && \
+axion RMX1901 user gms pico && \
+make installclean && \
+curl -sSL https://raw.githubusercontent.com/Shravan55555/Signing-Script/main/keygen.sh | bash && \
+ax -br
